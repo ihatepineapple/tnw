b9a9823a41b67bca3806137530fc762bc7b58cc9
ihatepineapple <ihatepineapple@gmail.com>
2022-01-19 20:29:28 +0100

    connected to api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./styles/styles.css";
 
 function App() {
+  const [ticketData, setTicketData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`https://next.tnw-staging.com/next-api/tickets.json`)
+      .then((tickets) => setTicketData(tickets.data.data))
+      .catch((error) => console.log(`${error}`));
+  }, []);
+
+  console.log(ticketData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>tnw</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ReactBreakpoints from "react-breakpoints";
+
+const breakpoints = {
+  mobile: 320,
+  mobileLandscape: 480,
+  tablet: 768,
+  tabletLandscape: 1024,
+  desktop: 1200,
+  desktopLarge: 1500,
+  desktopWide: 1920,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ReactBreakpoints breakpoints={breakpoints}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ReactBreakpoints>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
