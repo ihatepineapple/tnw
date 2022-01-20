56742107adb6da06c9da903aada2bf68ded072d0
ihatepineapple <ihatepineapple@gmail.com>
2022-01-20 21:02:04 +0100

    styles for header ok

--- src/components/ticket_header.js
+++ src/components/ticket_header.js
@@ -2,13 +2,31 @@ import React from "react";
 
 function HeaderTicket({ ticket }) {
   console.log(ticket);
+
+  const renderPrice = () => {
+    if (ticket.ticketStrikethroughPrice !== null) {
+      return (
+        <h4 className="price">
+          <span className="price__strikethrough">
+            € {ticket.ticketStrikethroughPrice}
+          </span>
+          <span className="price__offer">€ {ticket.ticketActualPrice}</span>
+        </h4>
+      );
+    } else {
+      return <h4 className="price">€ {ticket.ticketActualPrice}</h4>;
+    }
+  };
+
   return (
     <div>
-      <h3>{ticket.ticketName}</h3>
-      <p>{ticket.ticketDescription}</p>
+      <h2 className="ticket__title">{ticket.ticketName}</h2>
+      <p className="ticket__description">{ticket.ticketDescription}</p>
 
-      <h4>{ticket.ticketActualPrice}</h4>
-      <p>ex 21% VAT</p>
+      {renderPrice()}
+      <p className="price__footnote">ex 21% VAT</p>
+
+      <button className="btn">Register now</button>
     </div>
   );
 }
