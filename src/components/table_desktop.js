79a241d6293da7848219a07776e2d7ffb775eebe
ihatepineapple <ihatepineapple@gmail.com>
2022-01-22 20:50:08 +0100

    response array ok on function

--- src/components/table_desktop.js
+++ src/components/table_desktop.js
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from "react";
 
 function TableDesktop({ ticket, ticketData, text }) {
   const [perksList, setPerksList] = useState([]);
-  //   console.log(ticket);
+  const [checkmarkList, setCheckmarkList] = useState([]);
 
   useEffect(() => {
     setPerksList(getPerksList());
   }, [ticketData]);
-  //   console.log(ticket);
 
   const getPerksList = () => {
     let perksArray = [];
@@ -18,37 +17,49 @@ function TableDesktop({ ticket, ticketData, text }) {
           perksArray.push(ticketData[i].ticketPerks[j].ticketPerk);
         }
       }
-      //   console.log(perksArray);
     }
     return [...new Set(perksArray)];
   };
 
   const listItems = perksList.map((perk, index) => <li key={index}>{perk}</li>);
 
-  const checkmarkList = () => {
-    const ticketPerks = ticket.ticketPerks;
-    let response;
+  const ticketPerks = ticket.ticketPerks;
 
-    // perksList.map((perk, index) => {
-    //   for (let i = 0; i < ticketPerks.length; i++) {
-    //     if (
-    //       perk === ticketPerks[i].ticketPerk &&
-    //       ticketPerks[i].perkActive === "1"
-    //     ) {
-    //       console.log(ticket.ticketName + perk + " X");
-    //     } else {
-    //       console.log(ticket.ticketName + perk + " NOT included");
-    //     }
-    //   }
-    // });
+  const getResponseArray = () => {
+    const filteredTicketPerks = new Set(
+      ticketPerks
+        .filter(({ perkActive }) => perkActive)
+        .map(({ ticketPerk }) => ticketPerk)
+    );
+    const responseArray = perksList.map((perk) =>
+      filteredTicketPerks.has(perk)
+    );
+    console.log(responseArray);
+    // setCheckmarkList(responseArray);
+    // console.log(checkmarkList);
   };
 
   return (
     <div>
       <ul>{listItems}</ul>
-      <span>{checkmarkList()}</span>
+      <span>{getResponseArray()}</span>
     </div>
   );
 }
 
 export default TableDesktop;
+
+// const perksArray = ["blue", "pink", "red", "green", "yellow", "orange", "white"]
+
+// const ticketPerks = [{name:"pink", value: "1"}, {name:"green", value: null}, {name: "white", value: "1"}]
+
+// const checkedPerks = new Set(
+//   ticketPerks
+//     .filter(({value}) => value)
+//     .map(({name}) =>name)
+// )
+
+// console.log(checkedPerks)
+
+// const response = perksArray.map(perk => checkedPerks.has(perk))
+// console.log(response)
