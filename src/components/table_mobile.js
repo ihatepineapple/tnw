c40be110603c8e5e708f87d98f432a9aa4a7703b
ihatepineapple <ihatepineapple@gmail.com>
2022-01-23 15:56:05 +0100

    desktop ok again

---
+++ src/components/table_mobile.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import LinesEllipsis from "react-lines-ellipsis";
+
+function TableMobile({ ticket }) {
+  const [perksListMobile, setPerksListMobile] = useState([]);
+
+  useEffect(() => {
+    setPerksListMobile(getResponseArrayMobile());
+  }, [ticket]);
+
+  const getResponseArrayMobile = () => {
+    const ticketPerks = ticket.ticketPerks;
+
+    const filteredTicketPerks = new Set(
+      ticketPerks
+        .filter(({ perkActive }) => perkActive)
+        .map(({ ticketPerk }) => ticketPerk)
+    );
+    // setPerksListMobile(filteredTicketPerks);
+    return [...new Set(filteredTicketPerks)];
+  };
+
+  console.log(perksListMobile);
+
+  const listItemsMobile =
+    perksListMobile &&
+    perksListMobile.map((perk, index) => (
+      <li key={index}>
+        <LinesEllipsis
+          text={perk}
+          maxLine="1"
+          ellipsis="..."
+          trimRight
+          basedOn="letters"
+        />
+      </li>
+    ));
+
+  return (
+    <div>
+      <ul className="list_perks list_perks--centered">{listItemsMobile} </ul>
+    </div>
+  );
+}
+
+export default TableMobile;
